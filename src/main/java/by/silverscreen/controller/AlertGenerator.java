ea7c1cfc98b1a6020eb6cf458667e7e1ac529ed6
Skydance97 <skydance97@gmail.com>
2018-09-19 16:41:05 +0300

    Create alert generator

---
+++ src/main/java/by/silverscreen/controller/AlertGenerator.java
@@ -0,0 +1,14 @@
+package by.silverscreen.controller;
+
+import javafx.scene.control.Alert;
+
+public class AlertGenerator {
+
+    public static void showAlert(Alert.AlertType type, String title, String header, String content) {
+        Alert alert = new Alert(type);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
+}
